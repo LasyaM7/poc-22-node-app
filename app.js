2d6a020d550061cf6736ab498e2ec431e12a17fa
LasyaM7 <lasyamedikonda07@gmail.com>
2026-06-01 00:06:10 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const PORT = 3000;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('CI/CD Pipeline Working Successfully!');
+  res.end(Updated by GitHub Actions!);
 });
 
 server.listen(PORT, () => {
